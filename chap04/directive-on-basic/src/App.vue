e62c0e2f39a53f4202914e2ac00f8884b9064dd6
iruma-tea <shusuke.think.pad@gmail.com>
2024-04-07 15:11:23 +0900

    directive-on-basicの学習完了

---
+++ chap04/directive-on-basic/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const randValue = ref('まだです。');
+const onButtonClick = ():void => {
+  const rand = Math.round(Math.random() * 10);
+  randValue.value = String(rand);
+};
+</script>
+
+<template>
+  <button v-on:click="onButtonClick">クリック</button>
+  <p>クリックの結果{{ randValue }}</p>
+</template>
+
+<style>
+</style>
