1cbe47ad3c2e2da331c4a4c4747db5d66b641ec7
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-04-14 19:24:52 +0900

    components-props-bindの学習完了

---
+++ chap08/components-props-bind/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from "vue"
+import OneInfo from "./components/OneInfo.vue";
+
+const propsTitle = ref("発生した乱数");
+const rand = Math.round(Math.random() * 100);
+const propsContent = ref(rand);
+
+
+</script>
+
+<template>
+  <h2>テンプレート変数を利用</h2>
+  <section>
+    <OneInfo v-bind:title="propsTitle" v-bind:content="propsContent" />
+  </section>
+</template>
+
+<style scoped>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
