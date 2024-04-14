a571b6bfe252412cfaac8b37d4c2a921e62f568a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-04-14 18:58:04 +0900

    components-vmodelの学習完了

--- chap08/components-vmodel/src/App.vue
+++ chap08/components-vmodel/src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import WithModel from "./components/WithModel.vue";
 </script>
 
 <template>
-  <main>
-  </main>
+  <h1>コンポーネント独立性</h1>
+  <section>
+    <h2>v-modelを含むコンポーネント</h2>
+    <WithModel />
+    <WithModel />
+  </section>
 </template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
