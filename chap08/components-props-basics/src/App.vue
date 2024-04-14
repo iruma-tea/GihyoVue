f49e4216b8d7567f3a883463212bd266a7ae6dcb
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-04-14 19:12:16 +0900

    components-props-basicsの学習完了

---
+++ chap08/components-props-basics/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import OneInfo from "./components/OneInfo.vue"
+</script>
+
+<template>
+  <h1>Props基礎</h1>
+  <OneInfo title="Propsの利用" content="子コンポーネントにデータを渡すにはPropsを利用する"></OneInfo>
+</template>
+
+<style>
+section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
