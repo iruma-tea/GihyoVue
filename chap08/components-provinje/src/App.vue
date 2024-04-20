4d3af30a4fd27daa87736cfa82b15252f75aded4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-04-20 09:48:27 +0900

    components-provinjeの学習完了

---
+++ chap08/components-provinje/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {reactive, provide} from "vue";
+import BaseSection from "./components/BaseSection.vue";
+import type {Member} from "./interfaces";
+
+const memberList = new Map<number, Member>();
+memberList.set(33465, {id: 33465, name: "田中太郎", email: "bow@example.com", points: 35, note: "初回入会特権あり。"});
+memberList.set(47783, {id: 47783, name: "鈴木二郎", email: "mue@example.com", points: 53 });
+provide("memberList", reactive(memberList));
+</script>
+
+<template>
+  <BaseSection />
+</template>
