68ff1a760debac69f6a38e82ad97557b4f250615
iruma-tea <shusuke.think.pad@gmail.com>
2024-05-03 19:29:35 +0900

    router-multipleの学習完了

---
+++ chap10/router-multiple/src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import AppTop from '@/views/AppTop.vue';
+import Sub from '@/views/Sub.vue';
+
+const routeSettings: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "AppTop",
+    components: {
+      default: AppTop,
+      sub: Sub
+    }
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routeSettings
+})
+
+export default router
---
+++ chap10/router-multiple/src/views/Sub.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>サブコンテツです。</p>
+</template>
