71f700a572a08a73540f51ccf58fe6bbd1875da8
iruma-tea <shusuke.think.pad@gmail.com>
2024-05-04 12:51:43 +0900

    async-dbの学習開始

---
+++ chap12/async-db/src/stores/members.ts
@@ -0,0 +1,58 @@
+import { defineStore } from "pinia";
+import type { Member } from "@/interfaces";
+
+interface State {
+    memberList: Map<number, Member>;
+}
+
+export const useMembersStore = defineStore({
+    id: "members",
+    state: (): State => {
+        return {
+            memberList: new Map<number, Member>()
+        };
+    },
+    getters: {
+        getById: (state) => {
+            return (id: number): Member => {
+                const member = state.memberList.get(id) as Member;
+                return member;
+            };
+        },
+        isMemberListEmpty: (state): boolean => {
+            return state.memberList.size == 0;
+        }
+    },
+    actions: {
+        prepareMemberList(): void {
+            // 空のmemberListを用意
+            let memberList = new Map<number, Member>();
+            // セッションストレージからデータを取得
+            const memberListJSONStr = sessionStorage.getItem("memberList");
+            // セッションストレージのデータが空じゃないなら
+            if (memberListJSONStr != undefined) {
+                // JSON文字列をJSONオブジェクトに変換
+                const memberListJSON = JSON.parse(memberListJSONStr);
+                // JSONオブジェクトをもとにmemberListを生成
+                memberList = new Map<number, Member>(memberListJSON);
+            }
+            // ステートにmemberListを格納
+            this.memberList = memberList;
+        },
+        insertMember(member: Member): void {
+            // ステートの会員情報に追加
+            this.memberList.set(member.id, member);
+            // ステートの会員情報をJSON文字列に変換
+            const memberListJSONStr = JSON.stringify([...this.memberList]);
+            // セッションストレージに格納
+            sessionStorage.setItem("memberList", memberListJSONStr);
+        },
+        initList(): void {
+            this.memberList.set(33456, {id: 33456, name: "田中太郎", email: "bow@example.com", points: 35, note:"初回入会特権あり。"})
+            this.memberList.set(44783, {id: 44873, name:"鈴木次郎", email: "mue@example.com", points: 53});
+        },
+        addMember(member: Member): void {
+            this.memberList.set(member.id, member);
+        }
+    }
+});
