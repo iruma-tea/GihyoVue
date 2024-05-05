7deec6c1b30c02387415f6e735f1e25df9f4b491
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-05-05 19:51:18 +0900

    utest-inputの学習完了

--- chap13/utest-input/src/__tests__/App.spec.ts
+++ chap13/utest-input/src/__tests__/App.spec.ts
@@ -11,6 +11,16 @@ describe("App.vueのテスト",
                 const expected = "2";
                 expect(actual).toBe(expected);
             }
+        );
+        test("分子8で分母が2を入力した場合のテスト",
+            async () => {
+                const wrapper = mount(App);
+                await wrapper.get(`[data-testid="num1"`).setValue(8);
+                await wrapper.get(`[data-testid="num2"`).setValue(2);
+                const actual = wrapper.get(`[data-testid="ans"`).text();
+                const expected = "4";
+                expect(actual).toBe(expected);
+            }
         )
     }
-)
+);
