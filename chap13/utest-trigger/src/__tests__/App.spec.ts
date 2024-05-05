7cdd3f93e675505cc83d314e2147f961b322d287
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-05-06 06:21:15 +0900

    utest-triggerの学習完了

---
+++ chap13/utest-trigger/src/__tests__/App.spec.ts
@@ -0,0 +1,25 @@
+import { describe, test, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import App from '@/App.vue';
+
+describe("App.vueのテスト",
+    () => {
+        test("初期状態(隠し領域非表示のテスト)",
+            () => {
+                const wrapper = mount(App);
+                const actual = wrapper.find(`[data-testid="isVisible"]`).exists();
+                const expected = false;
+                expect(actual).toBe(expected);
+            }
+        );
+        test("表示ボタンをクリックした場合のテスト",
+            async () => {
+                const wrapper = mount(App);
+                await wrapper.get(`[data-testid="showButton"]`).trigger("click");
+				const actual = wrapper.find(`[data-testid="invisible"]`).exists();
+                const expected = true;
+                expect(actual).toBe(expected);
+           }
+        );
+    }
+);
